e9040b382b389e7e1674092491b3c3f3bdad1813
lil-D27 <dhanyaganesan2704@gmail.com>
2024-07-04 22:35:07 +0530

    Create practice.py

---
+++ 04. Randomization and python lists/practice.py
@@ -0,0 +1,21 @@
+import random
+#generates a random number
+
+#generates a random number
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# module - large piece of code is splitted into small blocks of code. 
+
+
+#importing our module
+import module
+print(module.pi)
+
+#0.0000000 - 0.9999999
+#generating random floating point number
+random_float = random.random()
+print(random_float)
+
+#generating random floating point number between 0 and 5
+random_float = random.random() * 5
