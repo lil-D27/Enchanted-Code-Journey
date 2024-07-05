124da13f705878135ff2e59569d64d82333c3f97
lil-D27 <dhanyaganesan2704@gmail.com>
2024-07-05 22:00:36 +0530

    Create Adding Evens.py

---
+++ 05. Python Loops/Adding Evens.py
@@ -0,0 +1,11 @@
+total = 0
+for number in range(0, 101, 2):
+  total += number
+print(total)
+
+total2 = 0
+for number in range(1, 101):
+  if number % 2 == 0:
+    total += number
+print(total2)
+  
